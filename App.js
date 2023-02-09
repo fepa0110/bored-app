fc6b90e0745c1479bbbb7d94de2bcd9c07680d1d
Teo-Fer <ismafer0007@gmail.com>
2023-02-09 08:30:05 -0300

    feat: :zap: [App.js] Se agrega traductor de idiomas

--- App.js
+++ App.js
@@ -1,41 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const primaryColor= "blue";
-const secondaryColor="";
-const primaryBackgroundColor="white";
-const secondBackgroundaryColor="";
+const primaryColor = "blue";
+const secondaryColor = "";
+const primaryBackgroundColor = "white";
+const secondBackgroundaryColor = "";
 const primaryTextColor = "white";
 const primaryIconColor = "blue";
 
-const url = "https://www.boredapi.com/api/activity/";
+const translateApi = "https://api.mymemory.translated.net/get?q=";
+//example translate api 
+//"https://api.mymemory.translated.net/get?q={text}!&langpair={fromLanguaje}|{toLanguaje}"
+const boredApi = "https://www.boredapi.com/api/activity/";
 export default function App() {
-  const [activity,setActivity] = useState({});
+  const [activity, setActivity] = useState({});
 
+  const fetchActivity = async () => {
+    const response = await fetch(boredApi);
+    const activity = await response.json();
+    const translatedActivity = await traslateActivity(activity.activity);
+    activity.activity = translatedActivity;
+    setActivity(activity);
+  };
 
-  const boredApiActivity = async () => {
-    const response = await fetch(url);
-    setActivity(await response.json())
-  }
-  //.json() https://www.boredresponse
+  const traslateActivity = async (text) => {
+    const response = await fetch(translateApi + text + "&langpair=en|es");
+    const tranlator = await response.json();
+    return tranlator.responseData.translatedText;
+  };
 
-  useEffect(()=>{
-    boredApiActivity()
-  },[])
-  
+  useEffect(() => {
+    fetchActivity();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.card}>
         <Text style={styles.activityTitle}>{activity.activity}</Text>
       </View>
 
-      <TouchableOpacity style={styles.btn} onPress={boredApiActivity}>
+      <TouchableOpacity style={styles.btn} onPress={fetchActivity}>
         <FontAwesome5 name="redo" size={30} color={primaryIconColor} />
       </TouchableOpacity>
     </View>
-
   );
 }
 
@@ -43,33 +52,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: primaryBackgroundColor,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   card: {
     borderRadius: 10,
     backgroundColor: primaryColor,
     width: "100%",
     height: "75%",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  activityTitle:{
-    fontSize:50,
-    fontWeight:"bold",
-    color:primaryTextColor,
+  activityTitle: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: primaryTextColor,
     fontStyle: "italic",
-    marginVertical: 5
+    marginVertical: 5,
   },
-  btn:{
+  btn: {
     margin: 10,
-    height:60,
-    width:60,
-    borderRadius:50,
+    height: 60,
+    width: 60,
+    borderRadius: 50,
     backgroundColor: primaryBackgroundColor,
-    alignItems:"center",
-    justifyContent:"center",
+    alignItems: "center",
+    justifyContent: "center",
     borderColor: primaryIconColor,
-    borderWidth:1,
-  }
+    borderWidth: 1,
+  },
 });
